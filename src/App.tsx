877f1a40a54d6261d30b83cacebe468d65c3d8f0
Wande Teniola <wandecreates@gmail.com>
2024-05-24 09:29:06 +0100

    i created the app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import leftSection from "./assets/images/left-section.png"
+
+function App() {
+  return (
+   <>
+   <div>
+    <img src={leftSection} alt="leftImage" className="h-full" />
+   </div>
+   </>
+  )
+}
+
+export default App;
